ed0fb3b5d70933069bf3edb3483bb44c2f0071a8
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-06-18 00:41:16 +0200

    feat: adds client-side routing

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import { useState } from 'react';
 import Tabbing from './components/navigation/Tabbing';
 import View from './components/View';
 
+function getCurrent () {
+  const current = location.hash.substring(1);
+  if (current === 'colors') {
+    return 'colors';
+  }
+  return 'clock';
+}
+
 export default function App () {
+  const current = getCurrent();
   const [viewState, setViewState] = useState({
-    current: 'clock'
+    current
   });
+  location.replace(`#${current}`);
   const updateView = function (current) {
     setViewState({ current });
-    // TODO: change url, read initial view from url on page load
+    location.replace(`#${current}`);
   };
   return (
     <main>
