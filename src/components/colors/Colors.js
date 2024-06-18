a6eddfbc31756deb84f8143868cea11ea1e1a2a0
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-06-18 13:54:47 +0200

    feat: improves design

--- src/components/colors/Colors.js
+++ src/components/colors/Colors.js
@@ -11,12 +11,12 @@ export default function Colors () {
 
       The shortest answer is: Color Wheel.
 
-      <p>
+      <div className="paragraph">
         This is a color wheel, with all colors on the rainbow.<br />
         <img src="img/wheel.png" />
-      </p>
+      </div>
 
-      <p>
+      <div className="paragraph">
         It's one of the many methods to choose and represent a color,
         so in hsl color space the angle on the wheel is used as hue, together with a saturation and a lightness value.<br />
         Example 1: <span style={{ background: 'hsl(0, 100%, 25%)', color: 'white' }}>Dark red</span> will be 0° hue on the wheel with 100% saturation and 25% lightness.<br />
@@ -27,29 +27,29 @@ export default function Colors () {
         Example 5: <span style={{ background: 'black', color: 'white' }}>Black</span> will be any hue with any saturation and 0% lightness.<br />
         Example 5: <span style={{ backgroundImage, color: 'black' }}>Shapes of gray</span> will be any hue with 0% saturation and the respective lightness
         (between 10% and 90% in this example).
-      </p>
+      </div>
 
-      <p>
+      <div className="paragraph">
         The colors of the clock on this page are based on the position/direction on the color wheel,
         so a minute-hand will be yellow if it's facing to 45° (showing 2, meaning it's x:10).<br />
         Another example: On 8:08:08 all hands will be blue, because they're facing to 225°.
-      </p>
+      </div>
 
-      <p>
+      <div className="paragraph">
         Here is an animation, showing the relation between the clock and the color wheel.<br />
         <div id="animation">
           <img src="img/clock.png" />
           <img id="wheel" src="img/wheel.png" />
         </div>
-      </p>
+      </div>
 
-      <p>
+      <div className="paragraph">
         To differentiate am and pm, the lighness is set as follows:<br />
         am &minus; <span style={{ color: 'hsl(0, 100%, 75%)' }}>milky</span> <span style={{ color: 'hsl(240, 100%, 75%)' }}>colors</span> (75% lightness)<br />
         pm &minus; <span style={{ color: 'hsl(0, 100%, 50%)' }}>pure</span> <span style={{ color: 'hsl(240, 100%, 50%)' }}>colors</span> (50% lightness)
-      </p>
+      </div>
 
-      <p>The saturation is always set to 100%.</p>
+      <div className="paragraph">The saturation is always set to 100%.</div>
     </main>
   );
 }
