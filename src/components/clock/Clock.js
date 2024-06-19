1b0ca09c77db26f93fc9f56403cf90a20910d38c
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-06-19 23:37:46 +0200

    fix: fix ticking performance
    update interval was created many times

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -4,31 +4,21 @@ import Bars from './Bars';
 import Numbers from './Numbers';
 import Hand from './Hand';
 import Axis from './Axis';
-import { useState } from 'react';
 
 const HEADER = 30;
 const PADDING = 20;
 
-export default function Clock () {
+export default function Clock ({time}) {
   const size = Math.min(window.innerWidth - PADDING, window.innerHeight - PADDING - HEADER);
-  const [timeState, setTimeState] = useState({
-    time: new Date()
-  });
-  function updateTime () {
-    setTimeState({
-      time: new Date()
-    });
-  }
-  setInterval(updateTime, 100);
   return (
     <div id="clock-container">
       <svg id="clock" width={size} height={size} viewBox="0 0 913 913">
         <Face />
-        <Bars time={timeState.time} />
-        <Numbers time={timeState.time} />
-        <Hand time={timeState.time} type="hour" />
-        <Hand time={timeState.time} type="minute" />
-        <Hand time={timeState.time} type="second" />
+        <Bars time={time} />
+        <Numbers time={time} />
+        <Hand time={time} type="hour" />
+        <Hand time={time} type="minute" />
+        <Hand time={time} type="second" />
         <Axis />
       </svg>
     </div>
