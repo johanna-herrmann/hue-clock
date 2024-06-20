ad84946e26573be5b91d8aba77c6045fe70a1441
Johanna Herrmann <johanna.herrmann@mailbox.org>
2024-06-20 18:16:26 +0200

    rafactor: improves lifecycle implementation of update time interval

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -4,21 +4,33 @@ import Bars from './Bars';
 import Numbers from './Numbers';
 import Hand from './Hand';
 import Axis from './Axis';
+import { useState, useEffect } from 'react';
 
 const HEADER = 30;
 const PADDING = 20;
 
-export default function Clock ({time}) {
+export default function Clock () {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 100);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   const size = Math.min(window.innerWidth - PADDING, window.innerHeight - PADDING - HEADER);
   return (
     <div id="clock-container">
       <svg id="clock" width={size} height={size} viewBox="0 0 913 913">
         <Face />
-        <Bars time={time} />
-        <Numbers time={time} />
-        <Hand time={time} type="hour" />
-        <Hand time={time} type="minute" />
-        <Hand time={time} type="second" />
+        <Bars time={currentTime} />
+        <Numbers time={currentTime} />
+        <Hand time={currentTime} type="hour" />
+        <Hand time={currentTime} type="minute" />
+        <Hand time={currentTime} type="second" />
         <Axis />
       </svg>
     </div>
